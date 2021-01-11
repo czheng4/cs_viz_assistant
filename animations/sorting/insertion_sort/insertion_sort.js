94fd63689e6a63457df08df6e1eaf1680b78648c
RetnuhFeel <h_leef@yahoo.com>
2021-01-11 16:20:37 -0500

    Insertion sort animation tweaks and link correction

--- animations/sorting/insertion_sort/insertion_sort.js
+++ animations/sorting/insertion_sort/insertion_sort.js
@@ -22,7 +22,7 @@ class insertionSortAnimation {
     let y = 0;
 
     rect = new Rect(x, y, v.length * this.ss, this.ss, 0, v, "", "bottom", "h");
-    t_rect = new Rect(x, this.ss + this.ss, this.ss, this.ss, 1, ['x'], "tmp", "bottom", "h");
+    t_rect = new Rect(x, this.ss + this.ss, this.ss, this.ss, 1, [""], "tmp", "bottom", "h");
     rect.font = "20px Arial";
     t_rect.font = "20px Arial";
     this.ani.add_object(rect);
@@ -57,7 +57,7 @@ class insertionSortAnimation {
     fill_styles[0] = "lightblue";
     ani.add_sequence_ani({ 
       target: rect,
-      text: "Before looping through the array, the first element is sorted.",
+      text: "Before looping through the array, the first element is always sorted.",
       prop: {
         fade_in: true,
         fillStyle: deep_copy(fill_styles),
@@ -68,52 +68,27 @@ class insertionSortAnimation {
     ani.add_sequence_ani({pause:ANIMATION_TIME * 1});
     fill_styles[0] = "#DDDDDD";
     for(i = 1; i < v_size; i++){
-      tmp = v_c[i];
-      //console.log("tmp is " + tmp);
-      if(tmp < v_c[i - 1]){
-        fill_styles[i] = "lightblue";
-        fill_styles[i - 1] = "pink";
-        ani.add_sequence_ani({ 
-          target: rect,
-          text: "Save {}{} (array[{}])</span> into tmp since it is less than {}{} array[{}]</span>.".format(BLUE_SPAN, v_c[i], i, RED_SPAN, v_c[i - 1], (i-1)),
-          prop: {
-            fade_in: true,
-            fillStyle: deep_copy(fill_styles),
-            step: true,
-            time: 1
-          }
-        });
-        ani.add_sequence_ani({
-          target: rect,
-          prop: {
-            copy: {index1: i, rect: t_rect, index2: 0, h_scale: 0, w_scale: 0},
-            //fillStyle: "lightblue",
-            time: 1.5 * ANIMATION_TIME, 
-          }
-          //concurrence: true
-        });
-        ani.add_sequence_ani({
-          target: t_rect,
-          prop: {
-            fade_in: true,
-            fillStyle: "lightblue",
-            time: 1
-          }
-          //concurrence: true
-        });
-        fill_styles[i] = "#DDDDDD";
-        fill_styles[i-1] = "#DDDDDD";
-      }
-      /*ani.add_sequence_ani({
+      fill_styles[i] = "lightblue";
+      ani.add_sequence_ani({
+        target: rect,
+        text: "Save {}{} (array[{}])</span> into tmp.".format(BLUE_SPAN, v_c[i], i),
+        prop: {
+          copy: {index1: i, rect: t_rect, index2: 0, h_scale: 0, w_scale: 0},
+          fillStyle: deep_copy(fill_styles),
+          time: 1.5 * ANIMATION_TIME, 
+        }
+      });
+      ani.add_sequence_ani({
         target: t_rect,
         prop: {
-          text: tmp,
           fade_in: true,
           fillStyle: "lightblue",
           time: 1
         }
-      });*/
-      //ani.add_sequence_ani({pause:ANIMATION_TIME * 1});
+      });
+      fill_styles[i] = "#DDDDDD";
+
+      tmp = v_c[i];
       for(j = i; j >= 1 && tmp < v_c[j-1]; j--){
         fill_styles[j] = "lightblue";
         fill_styles[j - 1] = "pink";
@@ -141,21 +116,22 @@ class insertionSortAnimation {
       }
       if(j !== i){
         fill_styles[j] = "lightblue";
-        ani.add_sequence_ani({ 
-          target: rect,
+        ani.add_sequence_ani({
+          target: t_rect,
           text: "Insert {}tmp {}</span> into {}(array[{}])</span>.".format(BLUE_SPAN, tmp, BLUE_SPAN, j),
+          prop: { 
+            copy: {index1: 0, rect: rect, index2: j, h_scale: 0, w_scale: 0},
+            fillStyle: "lightblue",
+            time: 1.5 * ANIMATION_TIME, 
+          },
+          concurrence: true
+        });
+        ani.add_sequence_ani({
+          target: rect,
           prop: {
             fade_in: true,
             fillStyle: deep_copy(fill_styles),
-            step: true,
             time: 1
-          }
-        });
-        ani.add_sequence_ani({
-          target: t_rect,
-          prop: {
-            copy: {index1: 0, rect: rect, index2: j, h_scale: 0, w_scale: 0},
-            time: 1.5 * ANIMATION_TIME, 
           }
         });
         ani.add_sequence_ani({
@@ -173,7 +149,7 @@ class insertionSortAnimation {
         fill_styles[j - 1] = "lightblue";
         ani.add_sequence_ani({ 
           target: rect,
-          text: "Element {}{} array[{}]</span> already in place since it is greater than {}{} array[{}]</span>.".format(RED_SPAN, tmp, j, BLUE_SPAN, v_c[j-1], (j - 1)),
+          text: "Element {}{} array[{}]</span> is greater than {}{} array[{}]</span>.".format(RED_SPAN, tmp, j, BLUE_SPAN, v_c[j-1], (j - 1)),
           prop: {
             fade_in: true,
             fillStyle: deep_copy(fill_styles),
@@ -181,10 +157,29 @@ class insertionSortAnimation {
             time: 1
           }
         });
-        ani.add_sequence_ani({pause:ANIMATION_TIME * 1});
-        fill_styles[i] = "#DDDDDD";
+        fill_styles[i] = "lightblue";
         fill_styles[j - 1] = "#DDDDDD";
+        ani.add_sequence_ani({
+          target: rect,
+          prop: {
+            fade_in: true,
+            fillStyle: deep_copy(fill_styles),
+            time: 1
+          },
+          concurrence: true
+        });
+        ani.add_sequence_ani({
+          target: t_rect,
+          text: "Therefore restore {}tmp {}</span> into {}(array[{}])</span>.".format(BLUE_SPAN, tmp, BLUE_SPAN, j),
+          prop: {
+            copy: {index1: 0, rect: rect, index2: j, h_scale: 0, w_scale: 0},
+            fillStyle: "lightblue",
+            time: 1.5 * ANIMATION_TIME, 
+          }
+        });
       }
+      fill_styles[i] = "#DDDDDD";
+      ani.add_sequence_ani({pause:ANIMATION_TIME * 1});
     }
     for(i = 0; i < v_size; i++){ fill_styles[i] = "lightblue"; }
     ani.add_sequence_ani({
@@ -193,6 +188,7 @@ class insertionSortAnimation {
       prop: { 
         fade_in: true,
         fillStyle: deep_copy(fill_styles),
+        step: true,
         time: 1
       }
     });
